5cc76d460548115f4714c95b3efff6550f6da2cf
Kaizenx <tc@supahands.com>
2021-03-15 15:34:32 +0800

    amended readme to be clearer on the expected output as well as made the seed file more random

--- seed.py
+++ seed.py
@@ -5,15 +5,22 @@ from random import randint
 start = datetime.now()
 end = start+timedelta(days=60)
 
-for _ in range(10):
- value = randint(1, 10)
+# for _ in range(10):
+#  if _ % 2 == 0:
+#   value = randint(1, 12)
+#  else:
+#   value = randint(24, 48) 
 
-step = timedelta(hours=value)
+# step = timedelta(hours=value)
 
 result = []
 
 while start < end:
     result.append(start.strftime('%Y-%m-%d %H:%M:%S'))
+    high_rand = randint(24, 72)
+    low_rand = randint(5,18)
+    value = randint(low_rand, high_rand) 
+    step = timedelta(hours=value)
     start += step
 
 print(result)
